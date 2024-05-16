71e8440e51939628b7d5c813c7e7422bd0157b16
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-05-16 09:44:34 +0300

    Time: 6 ms (78.78%), Space: 13.7 MB (69.19%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        int lenScore = score.size();
+        vector<string> answer(lenScore);
+        priority_queue<pair<int, int>>
+            maxHeap; // max heap to store scores with indices
+
+        for (int i = 0; i < lenScore; ++i) {
+            maxHeap.push({score[i], i});
+        }
+
+        vector<string> rank = {"Gold Medal", "Silver Medal", "Bronze Medal"};
+
+        int number = 1;
+        while (!maxHeap.empty()) {
+            auto top = maxHeap.top();
+            maxHeap.pop();
+            int index = top.second;
+            if (number < 4) {
+                answer[index] = rank[number - 1];
+            } else {
+                answer[index] = to_string(number);
+            }
+            number++;
+        }
+
+        return answer;
+    }
+};

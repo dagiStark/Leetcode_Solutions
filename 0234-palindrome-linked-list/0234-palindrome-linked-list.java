49d066af1f7a54b1ebff9bcab6d918a9db699dde
dagiStark <160214504+dagiStark@users.noreply.github.com>
2026-03-27 09:54:32 +0300

    Time: 15 ms (9.93%), Space: 101.7 MB (8.28%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,34 +1,25 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+import java.util.Stack;
+
 class Solution {
     public boolean isPalindrome(ListNode head) {
-       Stack<Integer> stack = new Stack<>();
-       Queue<Integer> queue = new LinkedList<>();
-
-       ListNode curr = head;
-       while(curr != null){
-        stack.push(curr.val);
-        curr = curr.next;
-       }
-       ListNode currQ = head;
-       while(currQ != null){
-        queue.add(currQ.val);
-        currQ = currQ.next;
-       }
-       while(!stack.isEmpty() && !queue.isEmpty()){
-        if(stack.pop() != queue.remove()){
-            return false;
+        Stack<Integer> stack = new Stack<>();
+        
+        // 1. Push all values into stack
+        ListNode curr = head;
+        while (curr != null) {
+            stack.push(curr.val);
+            curr = curr.next;
         }
-       }
-
-       return stack.isEmpty() && queue.isEmpty();
+        
+        // 2. Compare while popping
+        curr = head;
+        while (curr != null) {
+            if (curr.val != stack.pop()) {
+                return false;
+            }
+            curr = curr.next;
+        }
+        
+        return true;
     }
 }
